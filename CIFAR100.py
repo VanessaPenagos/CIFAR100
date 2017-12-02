2b0a8314e6845d3d24c4a9aa6bf4823d0a5743b5
Nany262 <daniela-zuluaga@utp.edu.co>
2017-12-01 20:41:00 -0500

    Prueba1

--- CIFAR100.py
+++ CIFAR100.py
@@ -8,6 +8,8 @@ from keras.datasets import cifar100
 from keras.utils import np_utils
 import numpy as np
 from sklearn.model_selection import GridSearchCV
+import zmq
+
 
 def create_model(learning_rate=1e-3, dropout_rate=0.1, units=8):
     model = Sequential()
@@ -46,33 +48,44 @@ def create_model(learning_rate=1e-3, dropout_rate=0.1, units=8):
                   metrics=['accuracy'])
     return model
 
+
 (X_train, y_train), (X_test, y_test) = cifar100.load_data()
 X_train = X_train.astype(np.float32) / 255
 X_test = X_test.astype(np.float32) / 255
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 
-epochs = 1000
-batch_size = 32
+epochs = 1 #300
+batch_size = 5 #32
+port = "5000"
+training_size = X_train.shape[0]
 
-model = create_model()
-checkpoint = ModelCheckpoint('checkpoints/epoch_{epoch:02d}-valacc_{val_acc:.2f}.hdf5', period=100)
-model.fit(X_train, y_train,
-          validation_split=0.2,
-          epochs=epochs, batch_size=batch_size,
-          callbacks=[checkpoint])
-model.save('model.hdf5')
+context = zmq.Context()
+socket = context.socket(zmq.REP)
 
-#model = KerasClassifier(
-#    build_fn=create_model,
-#    epochs=5,
-#    steps_per_epoch=training_size//batch_size,
-#    verbose=0)
+socket.bind(f"tcp://*:{port}")
 
-#param_grid = {
-#    'learning_rate': [1e-4, 1e-3, 1e-2, 1e-1],
-#    'dropout_rate': [0.2, 0.3, 0.4, 0.5],
-#    'units': [4, 8, 16, 32]
-#}
-#grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1)
-#grid.fit(X_train, y_train)
+param_grid = socket.recv_json()
+
+model = KerasClassifier(
+    build_fn=create_model,
+    epochs=epochs,
+    steps_per_epoch=training_size // batch_size,
+    verbose=0)
+
+grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1)
+grid.fit(X_train[:5], y_train[:5])
+
+answer = {'best_score': grid.best_score_}
+socket.send_json(answer)
+
+grid.save(f'best_model_with_score_{grid.best_score_}')
+
+# PRUEBA SIN GRIDSEARCH
+# model = create_model()
+# checkpoint = ModelCheckpoint('checkpoints/epoch_{epoch:02d}-valacc_{val_acc:.2f}.hdf5', period=100)
+# model.fit(X_train, y_train,
+#           validation_split=0.2,
+#           epochs=epochs, batch_size=batch_size,
+#           callbacks=[checkpoint])
+# model.save('model.hdf5')
--- nodeMaster.py
+++ nodeMaster.py
@@ -1,28 +1,28 @@
 import zmq
 
-address = [
-    "127.0.0.1:5001", "127.0.0.1:5002", "127.0.0.1:5003", "127.0.0.1:5004",
-    "127.0.0.1:5005"
-]
+address = ["192.168.1.200:5000", "192.168.1.225:5000"]
 
 param_grid = {
-    'learning_rate': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1],
-    'dropout_rate': [0.2, 0.3, 0.4, 0.5],
-    'units': [4, 8, 16, 32]
+    'learning_rate': [1e-3, 1e-2],
+    'dropout_rate': [0.2, 0.4, 0.8],
+    'units': [4, 8, 16]
 }
 
 answers = {}
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 
-for i in range(len(address)-1):
+for i in range(len(address)):
     socket.connect(f"tcp://{address[i]}")
     message = {
-        'learning_rate': [param_grid['learning_rate'][i]],
-        'dropout_rate': param_grid['dropout_rate'],
-        'units': param_grid['units']
+        'learning_rate': [param_grid['learning_rate']],
+        'dropout_rate': [param_grid['dropout_rate'][i]],
+        'units': [param_grid['units']]
     }
 
     socket.send_json(message)
     answers[address[i]] = socket.recv_json()
     print("Finish him!! of: ", address[i])
+
+    best_score = max(answers.keys())
+    print(f"The best score is: {best_score}")
